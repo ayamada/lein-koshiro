6d82a14697def5fc3cef988e4a0889b0e61ebb30
atsuo yamada <yamada-git@tir.jp>
2016-02-25 13:11:31 +0900

    Released 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject jp.ne.tir/lein-koshiro "0.1.0"
+(defproject jp.ne.tir/lein-koshiro "0.1.1-SNAPSHOT"
   :description "Yet another lein-ancient"
   :url "https://github.com/ayamada/lein-koshiro"
   :license {:name "MIT License"
