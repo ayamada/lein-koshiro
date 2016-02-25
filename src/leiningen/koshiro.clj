da9d5fdc86be04f678ffb59ff2442b09c80b5449
atsuo yamada <yamada-git@tir.jp>
2016-02-25 17:30:10 +0900

    Add deep check for stable/qualified/snapshot version

--- src/leiningen/koshiro.clj
+++ src/leiningen/koshiro.clj
@@ -1,30 +1,52 @@
 (ns leiningen.koshiro
-  (:require [ancient-clj.core :as ancient]
-            [project-clj.core]))
+  (:require [project-clj.core :as project-clj]
+            [ancient-clj.core :as ancient-clj]
+            [version-clj.core :as version-clj]))
 
-(defn- get-dependencies-from-project-clj []
-  (eval '(apply concat
-                (project-clj.core/get :plugins)
-                (project-clj.core/get :dependencies)
-                (map (fn [[k v]]
-                       (concat (:plugins v) (:dependencies v)))
-                     (project-clj.core/get :profiles)))))
+(defn- get-all-dependencies-from-project-clj []
+  (eval `(apply concat
+                (project-clj/get :plugins)
+                (project-clj/get :dependencies)
+                (map (fn [[k# v#]]
+                       (concat (:plugins v#) (:dependencies v#)))
+                     (project-clj/get :profiles)))))
+
+(defn- get-latest-version [artifact-id options]
+  (ancient-clj/latest-version-string! artifact-id options))
 
 (defn koshiro
   "Check version of all :dependencies (include :profiles) in your project.clj"
   [project & args]
-  (doseq [dependency (get-dependencies-from-project-clj)]
+  (doseq [dependency (get-all-dependencies-from-project-clj)]
     (let [[artifact-id current-version & dep-opts] dependency
-          ;; TODO: customize by args
-          ancient-options {:snapshots? false
-                           :qualified? false
-                           ;:repositories {...}
-                           }
-          latest-version (ancient/latest-version-string! artifact-id
-                                                         ancient-options)
-          target (pr-str [artifact-id current-version])
-          result (if (= current-version latest-version)
-                   "(latest-stable)"
-                   (str (pr-str [artifact-id latest-version]) " !!!"))]
-      (println target "=>" result)))
+          target (pr-str (if (empty? dep-opts)
+                           [artifact-id current-version]
+                           [artifact-id current-version '...]))]
+      (print target "")
+      (let [include-snapshot? (version-clj/snapshot? current-version)
+            include-qualified? (version-clj/qualified? current-version)
+            latest-label (cond
+                           include-snapshot? "(latest-snapshot)"
+                           include-qualified? "(latest-qualified)"
+                           :else "(latest-stable)")
+            options {:snapshots? include-snapshot?
+                     :qualified? true
+                     ;:repositories {...} ; TODO: find from project
+                     }
+            latest-version (get-latest-version artifact-id options)
+            result (cond
+                     (= current-version latest-version) latest-label
+                     include-snapshot? (pr-str latest-version)
+                     include-qualified? (pr-str latest-version)
+                     ;; deep check for stable version
+                     :else (let [latest-qualified latest-version
+                                 options (assoc options :qualified? false)
+                                 latest-stable (get-latest-version
+                                                 artifact-id options)]
+                             (if (= current-version latest-stable)
+                               (str "(latest-stable, but found "
+                                    (pr-str latest-qualified)
+                                    ")")
+                               (pr-str latest-stable))))]
+        (println "=>" result))))
   (println "All done."))
