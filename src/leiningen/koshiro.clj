31ba93cf1f9c1fa061d1179bda5a2165645febe7
atsuo yamada <yamada-git@tir.jp>
2016-02-26 09:14:50 +0900

    Refactor deep check code

--- src/leiningen/koshiro.clj
+++ src/leiningen/koshiro.clj
@@ -14,6 +14,30 @@
 (defn- get-latest-version [artifact-id options]
   (ancient-clj/latest-version-string! artifact-id options))
 
+(defn- get-version-type [ver]
+  (cond
+    (version-clj/snapshot? ver) :snapshot
+    (version-clj/qualified? ver) :qualified
+    :else :stable))
+
+(defn- matched-message [version-type]
+  (case version-type
+    :stable "(latest-stable)"
+    :qualified "(latest-qualified)"
+    :snapshot "(latest-snapshot)"))
+
+(defn- af! [current-version latest-version]
+  (throw (ex-info "assertion failed" {:current-version current-version
+                                      :latest-version latest-version})))
+
+(defn- deep-check [artifact-id options current-version latest-version]
+  (let [latest-qualified latest-version
+        options (assoc options :qualified? false)
+        latest-stable (get-latest-version artifact-id options)]
+    (if (= current-version latest-stable)
+      (str "(latest-stable, but found " (pr-str latest-qualified) ")")
+      (pr-str latest-stable))))
+
 (defn koshiro
   "Check version of all :dependencies (include :profiles) in your project.clj"
   [project & args]
@@ -23,30 +47,28 @@
                            [artifact-id current-version]
                            [artifact-id current-version '...]))]
       (print target "=>" "")
-      (let [include-snapshot? (version-clj/snapshot? current-version)
-            include-qualified? (version-clj/qualified? current-version)
-            latest-label (cond
-                           include-snapshot? "(latest-snapshot)"
-                           include-qualified? "(latest-qualified)"
-                           :else "(latest-stable)")
-            options {:snapshots? include-snapshot?
+      (let [current-version-type (get-version-type current-version)
+            options {:snapshots? (= current-version-type :snapshot)
                      :qualified? true
                      ;:repositories {...} ; TODO: find from project
                      }
             latest-version (get-latest-version artifact-id options)
-            result (cond
-                     (= current-version latest-version) latest-label
-                     include-snapshot? (pr-str latest-version)
-                     include-qualified? (pr-str latest-version)
-                     ;; deep check for stable version
-                     :else (let [latest-qualified latest-version
-                                 options (assoc options :qualified? false)
-                                 latest-stable (get-latest-version
-                                                 artifact-id options)]
-                             (if (= current-version latest-stable)
-                               (str "(latest-stable, but found "
-                                    (pr-str latest-qualified)
-                                    ")")
-                               (pr-str latest-stable))))]
+            latest-version-type (get-version-type latest-version)
+            result (if (= current-version latest-version)
+                     (matched-message current-version-type)
+                     (case [current-version-type latest-version-type]
+                       [:stable :stable] (pr-str latest-version)
+                       [:stable :qualified] (deep-check artifact-id
+                                                        options
+                                                        current-version
+                                                        latest-version)
+                       [:stable :snapshot] (af! current-version latest-version)
+                       [:qualified :stable] (pr-str latest-version)
+                       [:qualified :qualified] (pr-str latest-version)
+                       [:qualified :snapshot] (af! current-version
+                                                   latest-version)
+                       [:snapshot :stable] (pr-str latest-version)
+                       [:snapshot :qualified] (pr-str latest-version)
+                       [:snapshot :snapshot] (pr-str latest-version)))]
         (println result))))
   (println "All done."))
