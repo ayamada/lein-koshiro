a4cb6245a0f7c040deb6d95aa81e9b8856916010
atsuo yamada <yamada-git2@tir.jp>
2017-11-13 17:45:40 +0900

    Fix for leiningen-2.8.1

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject jp.ne.tir/lein-koshiro "0.1.6-SNAPSHOT"
-  :min-lein-version  "2.7.0"
+  :min-lein-version  "2.8.1"
   :description "Yet another lein-ancient"
   :url "https://github.com/ayamada/lein-koshiro"
   :license {:name "MIT License"
@@ -7,13 +7,14 @@
             :year 2016
             :key "mit"}
   ;; for test :managed-dependencies
-  ;:managed-dependencies [[jp.ne.tir/project-clj "0.1.6"]]
-  :dependencies [[jp.ne.tir/project-clj "0.1.6"
+  ;:managed-dependencies [[jp.ne.tir/project-clj "0.1.7"]]
+  :dependencies [[jp.ne.tir/project-clj "0.1.7"
                   :exclusions [org.codehaus.plexus/plexus-utils
                                org.apache.maven.wagon/wagon-provider-api]]
-                 [ancient-clj "0.3.14" :exclusions [org.clojure/clojure]]
-                 [version-clj "0.1.2" :exclusions [org.clojure/clojure]]]
-  ;; :pedantic? :abort
+                 [ancient-clj "0.6.14" :exclusions [org.clojure/clojure org.apache.httpcomponents/httpclient]]
+                 [version-clj "0.1.2" :exclusions [org.clojure/clojure]]
+                 [slingshot "0.12.2"]]
+  ;:pedantic? :abort
   :eval-in-leiningen true
   ;; These profiles are for test run
   :profiles {:for-localtest {:plugins [[lein-midje "3.1"]
